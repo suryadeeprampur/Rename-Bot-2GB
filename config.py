62159d019396678974e3245e8867a5856da3167c
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-06-18 12:06:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -106,16 +106,6 @@ For Example :-
 
 
 
-WORKDIR /app
-COPY . /app/
-
-# Install dependencies
-RUN apt-get update && apt-get install -y ffmpeg
-
-# Install Python requirements
-RUN pip3 install -r requirements.txt
-
-CMD ["python3", "bot.py"]
 
 
 
